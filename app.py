0b89a4c277702932acec45fa8ef07899425dcdf4
wndenis <wndenis@yandex.ru>
2020-05-16 07:53:31 +0300

    add db manipulation and some temp methods

--- app.py
+++ app.py
@@ -4,25 +4,61 @@
 import werkzeug
 werkzeug.cached_property = werkzeug.utils.cached_property
 # =======================================================
-from flask import Flask
+from flask import Flask, request
 from flask_restplus import Api, Resource
+import logging
 import db_test
+import pandas as pd
+
+
+
 app = Flask(__name__)
 api = Api(app)
+# todo: auth db using docker-compose
+logging.basicConfig(level=logging.DEBUG)
 
-# todo: I realized that I shouldnt try to connect to db at startup
 
-@api.route('/hello')
-class GelloWorld(Resource):
+@api.route("/users/")
+class Users(Resource):
+    @api.doc("List of all users", params={"id": "user ID"})
     def get(self):
-        return {"Hi": "world!"}
+        conn = app.db.conn
+        res = pd.read_sql_query(f'select * from users', con=conn).to_json()
+        return res
+
+    @api.doc("Create a new user", params={"name": "user name", "age": "user age"})
+    @api.response(201, 'User created successfully')
+    # @api.expect()
+    def post(self):
+        data = request.json
+        # todo: resources with classes
+        # todo: how to send data - in body or in header?
+        print(data)
+        # name = request.form["name"]
+        # age = request.form["age"]
+        # df = pd.DataFrame(data={'name': name, 'age': age})
+        # df.to_sql(name='users', con=app.db, if_exists='replace', index=False)
+
+
+
+    def put(self):
+        return "pat"
+
+
+    def delete(self):
+        return "del"
+
+@api.route("/users/<int:id>")
+class User(Resource):
+
+    def get(self, id):
+        conn = app.db.conn
+        res = pd.read_sql_query(f'select * from users where id={id}', con=conn).to_json()
+        app.logger.info(res)
+        return res
 
 
 if __name__ == '__main__':
-    try:
-        db_test.test_connection()
-    except Exception as e:
-        print(e)
-    else:
-        print("DB OK")
+    app.db = db_test.db()
+    app.db.recreate()
     app.run(host="0.0.0.0", debug=True)
--- db_test.py
+++ db_test.py
@@ -1,18 +1,45 @@
 import psycopg2
-import psycopg2 as pg
-import pandas.io.sql as psql
-import pandas as pd
+import time
 
-def test_connection():
-    # connection = pg.connect("host='db_docker' dbname=api_db user=postgres password='test'")
-    # df = pd.read_sql_query('select * from category', con=connection)
-    # print(df)
+db_boilerplate = """DROP TABLE IF EXISTS "users";
+DROP SEQUENCE IF EXISTS users_id_seq;
+CREATE SEQUENCE users_id_seq INCREMENT 1 MINVALUE 1 MAXVALUE 2147483647 START 1 CACHE 1;
 
-    # conn = psycopg2.connect(host="db_docker",
-    #                         # port=5432,
-    #                         dbname="api_db",
-    #                         user="postgres",
-    #                         password="test")
-    conn = psycopg2.connect("host=db_docker dbname=api_db user=postgres password=test")
+CREATE TABLE "public"."users" (
+    "id" integer DEFAULT nextval('users_id_seq') NOT NULL,
+    "name" character varying(80) NOT NULL,
+    "age" smallint NOT NULL
+) WITH (oids = false);
 
+INSERT INTO "users" ("id", "name", "age") VALUES
+(0,	'Peter Jackson',	42),
+(1,	'John Dow',	252),
+(2,	'Baba Dusya',	89),
+(3,	'Thomas the Train',	251),
+(4,	'Виктор Зубов',	22),
+(5,	'Дарья Игонина',	35),
+(6,	'Евгений Автобусов',	4);"""
+
+
+class db:
+    def __init__(self):
+        self.conn = None
+        self.establish_connection()
+
+    def establish_connection(self):
+        while not self.conn:
+            time.sleep(1)
+            try:
+                self.conn = psycopg2.connect(host="db_docker",
+                                        dbname="api_db",
+                                        user="postgres",
+                                        password="test")
+            except psycopg2.OperationalError as e:
+                pass
+
+    def recreate(self):
+        cur = self.conn.cursor()
+        cur.execute(db_boilerplate)
+        self.conn.commit()
+        cur.close()
 
