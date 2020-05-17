b4693274ae1a43fd7f23d64ce79c2bad9dec2cd2
wndenis <wndenis@yandex.ru>
2020-05-17 06:52:51 +0300

    wip

--- app.py
+++ app.py
@@ -7,9 +7,10 @@ werkzeug.cached_property = werkzeug.utils.cached_property
 from flask import Flask, request
 from flask_restplus import Api, Resource
 import logging
-import db_test
 import pandas as pd
 
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 
 app = Flask(__name__)
@@ -20,45 +21,51 @@ logging.basicConfig(level=logging.DEBUG)
 
 @api.route("/users/")
 class Users(Resource):
-    @api.doc("List of all users", params={"id": "user ID"})
+    # todo: docstring
+    @api.doc("List of all users")
     def get(self):
         conn = app.db.conn
-        res = pd.read_sql_query(f'select * from users', con=conn).to_json()
+        res = pd.read_sql_query(f'select * from users', con=conn).to_dict(orient="records")
         return res
 
     @api.doc("Create a new user", params={"name": "user name", "age": "user age"})
     @api.response(201, 'User created successfully')
     # @api.expect()
     def post(self):
-        data = request.json
-        # todo: resources with classes
-        # todo: how to send data - in body or in header?
+        # todo: how to send data - in json or in form?
+        data = request.json or request.form
+        if not data:
+            print("empty body")
         print(data)
-        # name = request.form["name"]
-        # age = request.form["age"]
-        # df = pd.DataFrame(data={'name': name, 'age': age})
-        # df.to_sql(name='users', con=app.db, if_exists='replace', index=False)
+        name = data["name"]
+        age = data["age"]
+        df = pd.DataFrame(data={'name': name, 'age': age}, index=[0])
+        print(df)
+        print()
+        # append because of POST
+        df.to_sql(name='users', con=app.db.conn, if_exists='append', index=False)
 
-
-
+    @api.doc("Change user", params={"id": "user id", "name": "user name", "age": "user age"})
     def put(self):
         return "pat"
-
 
     def delete(self):
         return "del"
 
 @api.route("/users/<int:id>")
 class User(Resource):
-
+    @api.doc("List of all users", params={"id": "user ID"})
     def get(self, id):
         conn = app.db.conn
-        res = pd.read_sql_query(f'select * from users where id={id}', con=conn).to_json()
+        res = pd.read_sql_query(f'select * from users where id={id}', con=conn).to_dict(orient="records")
         app.logger.info(res)
         return res
 
 
 if __name__ == '__main__':
+
+
+
     app.db = db_test.db()
     app.db.recreate()
     app.run(host="0.0.0.0", debug=True)
--- db_test.py
+++ db_test.py
@@ -1,5 +1,29 @@
-import psycopg2
 import time
+from sqlalchemy import create_engine, Column, Integer, String, DateTime
+from sqlalchemy.engine.url import URL
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+DATABASE = {
+    'drivername': 'postgres',
+    'host': 'db_docker',
+    'port': '5432',
+    'username': 'postgres',
+    'password': 'test',
+    'database': 'api_db'
+}
+
+
+def get_engine():
+    if get_engine.session:
+        return get_engine.session
+
+    engine = create_engine(URL(**DATABASE))
+    DeclarativeBase.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    return session
+
 
 db_boilerplate = """DROP TABLE IF EXISTS "users";
 DROP SEQUENCE IF EXISTS users_id_seq;
@@ -20,26 +44,31 @@ INSERT INTO "users" ("id", "name", "age") VALUES
 (5,	'Дарья Игонина',	35),
 (6,	'Евгений Автобусов',	4);"""
 
+DeclarativeBase = declarative_base()
 
-class db:
-    def __init__(self):
-        self.conn = None
-        self.establish_connection()
+class User(DeclarativeBase):
+    __tablename__ = "users"
 
-    def establish_connection(self):
-        while not self.conn:
-            time.sleep(1)
-            try:
-                self.conn = psycopg2.connect(host="db_docker",
-                                        dbname="api_db",
-                                        user="postgres",
-                                        password="test")
-            except psycopg2.OperationalError as e:
-                pass
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column("name", String)
+    age = Column("age", Integer)
 
-    def recreate(self):
-        cur = self.conn.cursor()
-        cur.execute(db_boilerplate)
-        self.conn.commit()
-        cur.close()
+    def __repr__(self):
+        return "".format(self.code)
+
+    # def establish_connection(self):
+    #     while not self.conn:
+    #         time.sleep(1)
+    #         try:
+    #             self.conn = psycopg2.connect(host="db_docker",
+    #                                     dbname="api_db",
+    #                                     user="postgres",
+    #                                     password="test")
+    #         except psycopg2.OperationalError as e:
+    #             print(e) # log?
+    #
+    # def recreate(self):
+    #     cur = self.conn.cursor()
+    #     cur.execute(db_boilerplate)
+    #     self.conn.commit()
 
