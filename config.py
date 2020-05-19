f428a926197b51223c1f6602fb9aa1e4df037220
wndenis <wndenis@yandex.ru>
2020-05-19 07:32:13 +0300

    Add config

---
+++ config.py
@@ -0,0 +1,15 @@
+import os
+
+# retrieve environment vars from docker-compose
+# so configuration made only once at deployment
+pg_host = os.environ["POSTGRES_HOST"]
+pg_port = os.environ["POSTGRES_PORT"]
+
+pg_user = os.environ["POSTGRES_USER"]
+pg_password = os.environ["POSTGRES_PASSWORD"]
+
+pg_db = os.environ["POSTGRES_DB"]
+
+DB_URI = f"postgresql+psycopg2://{pg_user}:{pg_password}@{pg_host}:{pg_port}/{pg_db}"
+
+fake_db = bool(os.environ["FAKE_DB"])
