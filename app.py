3b858e840a9a42716d1d21a098ec1268db147696
wndenis <wndenis@yandex.ru>
2020-05-19 07:37:01 +0300

    Add some API methods
    Add logging
    Add GET, POST at /users
    Add GET, PUT, DELETE at /users/id
    DAO based manipulation
    Add filling database with random values at startup
    Add workaround is database is not ready

--- app.py
+++ app.py
@@ -4,68 +4,174 @@
 import werkzeug
 werkzeug.cached_property = werkzeug.utils.cached_property
 # =======================================================
-from flask import Flask, request
-from flask_restplus import Api, Resource
-import logging
-import pandas as pd
+from flask import Flask
+import flask_sqlalchemy
+from flask_restplus import Api, Resource, fields
+from faker import Faker
+import random
+from logging import DEBUG
+from psycopg2 import OperationalError
+import time
 
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-
+import config
 
 app = Flask(__name__)
-api = Api(app)
-# todo: auth db using docker-compose
-logging.basicConfig(level=logging.DEBUG)
+api = Api(app, version="0.1", title="Sber test task API")
+
+user_api = api.model("User", {
+    "id": fields.Integer(readOnly=True, description="Unique id of user"),
+    "name": fields.String(required=True, description="User's name"),
+    "age": fields.Integer(required=True, description="User's age")
+})
+
+db = flask_sqlalchemy.SQLAlchemy()
+app.logger.setLevel(DEBUG)
+
+
+class UsersModel(db.Model):
+    """sqlalchemy model represents "Users" table"""
+
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column("name", db.String(80))
+    age = db.Column("age", db.Integer)
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    @property
+    def serialize(self):
+        return {"id": self.id,
+                "name": self.name,
+                "age": self.age}
+
+    def __repr__(self):
+        return f"<User {self.name} {self.age}>"
+
+
+class UsersDAO(object):
+    """data access object to interact with database"""
+    @property
+    def all(self):
+        result = UsersModel.query.all()
+        return [elem.serialize for elem in result]
+
+    def get(self, id):
+        user = UsersModel.query.filter_by(id=id).first()
+        if user:
+            print(user)
+            print(type(user))
+            return user.serialize
+        api.abort(404, f"User {id} not found")
+
+    def create(self, data):
+        user = UsersModel(data["name"], data["age"])
+        db.session.add(user)
+        db.session.commit()
+        return user.serialize
+
+    def update(self, id, data):
+        user = UsersModel.query.filter_by(id=id).first
+        # if parameter is present, we change user's parameter
+        # else leave the old one
+        user["name"] = data["name"] or user["name"]
+        user["age"] = data["age"] or user["age"]
+        db.session.commit()
+        return user.serialize
+
+
+    def delete(self, id):
+        user = UsersModel.query.filter_by(id=id).first()
+        db.session.delete(user)
+        db.session.commit()
+
+
+DAO = UsersDAO()
 
 
 @api.route("/users/")
-class Users(Resource):
+class UsersResource(Resource):
+    # """users api"""
     # todo: docstring
-    @api.doc("List of all users")
     def get(self):
-        conn = app.db.conn
-        res = pd.read_sql_query(f'select * from users', con=conn).to_dict(orient="records")
-        return res
+        """List of all users"""
+        # res = pd.read_sql_query(f'select * from users', con=conn).to_dict(orient="records")
+        return DAO.all
 
-    @api.doc("Create a new user", params={"name": "user name", "age": "user age"})
     @api.response(201, 'User created successfully')
-    # @api.expect()
+    @api.expect(user_api)
     def post(self):
-        # todo: how to send data - in json or in form?
-        data = request.json or request.form
-        if not data:
-            print("empty body")
-        print(data)
-        name = data["name"]
-        age = data["age"]
-        df = pd.DataFrame(data={'name': name, 'age': age}, index=[0])
-        print(df)
-        print()
-        # append because of POST
-        df.to_sql(name='users', con=app.db.conn, if_exists='append', index=False)
+        """Create a new user"""
+        # df = pd.DataFrame(data={'name': name, 'age': age}, index=[0])
+        # df.to_sql(name='users', con=app.db.conn, if_exists='append', index=False)
+        return DAO.create(api.payload), 201
 
-    @api.doc("Change user", params={"id": "user id", "name": "user name", "age": "user age"})
-    def put(self):
-        return "pat"
-
-    def delete(self):
-        return "del"
 
 @api.route("/users/<int:id>")
+@api.response(404, "User not found")
+@api.param("id", "User identifier")
 class User(Resource):
-    @api.doc("List of all users", params={"id": "user ID"})
+    @api.doc("get_user")
     def get(self, id):
-        conn = app.db.conn
-        res = pd.read_sql_query(f'select * from users where id={id}', con=conn).to_dict(orient="records")
-        app.logger.info(res)
-        return res
+        """Fetch user with given id"""
+        # res = pd.read_sql_query(f'select * from users where id={id}', con=conn).to_dict(orient="records")
+        return DAO.get(id)
+
+    @api.doc("change_user")
+    @api.marshal_with(user_api)
+    def put(self, id):
+        """Update user with given id"""
+        return DAO.update(id, api.payload)
+
+    @api.doc("delete_user")
+    @api.response(204, "User deleted")
+    def delete(self, id):
+        """Delete user with given id"""
+        DAO.delete(id)
+        return "", 204
+
+
+def recreate_db():
+    """
+    function for testing purposes: fills database with some entries if it's empty
+    """
+    # if database is not empty, skip this step
+    if UsersModel.query.count():
+        app.logger.info("Database is not empty, skipping recreating")
+        return
+    # else create 100 random entries
+    fake = Faker()
+    for x in range(100):
+        user = UsersModel(name=fake.name(), age=random.randint(1, 99))
+        db.session.add(user)
+        db.session.commit()
+        app.logger.info(f"Created {user}")
 
 
 if __name__ == '__main__':
+    app.config["SQLALCHEMY_DATABASE_URI"] = config.DB_URI
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    app.app_context().push()
+    db.init_app(app)
 
-
-
-    app.db = db_test.db()
-    app.db.recreate()
+    # I need this to avoid Operational Error which may be caused by long time of database loading
+    # try to connect 30 times with delay of 1 second
+    # else halt and print that we couldn't manage to connect to database
+    db_connected = False
+    for x in range(30):
+        try:
+            db.create_all()
+            db_connected = True
+            break
+        except OperationalError as e:
+            app.logger.info(e)
+            time.sleep(1)
+    if not db_connected:
+        app.logger.error("Couldn't connet to database")
+        exit(1)
+    app.logger.info("Database connected successfully")
+    if config.fake_db:
+        recreate_db()
     app.run(host="0.0.0.0", debug=True)
